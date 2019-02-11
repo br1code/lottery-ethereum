262c738090f2bc558eb1221ab239c05d59c0c4ba
br1code <brunogiovagnoli@gmail.com>
2019-02-11 13:05:17 -0300

    some refactor

--- src/App.js
+++ src/App.js
@@ -6,128 +6,131 @@ import { Button, Form, FormGroup, Label, Input, Container, Row, Col, Alert } fro
 
 class App extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      manager: '',
-      players: [],
-      balance: '',
-      value: '',
-      message: '',
-      color: '',
-      isManager: false
-    };
-  }
+  	constructor(props) {
+    	super(props);
+		
+		this.state = {
+			manager: '',
+			balance: '',
+			value: '',
+			message: '',
+			color: '',
+			players: [],
+			isManager: false
+		};
+  	}
 
-  componentDidMount() {
-    this.updateStats();
-  }
+	componentDidMount() {
+		setInterval(this.updateStats, 15000);
+	}
 
-  updateStats = async () => {
-    const manager = await lottery.methods.manager().call();
-    const players = await lottery.methods.getPlayers().call();
-    const balance = await web3.eth.getBalance(lottery.options.address);
+	updateStats = async () => {
+		const manager = await lottery.methods.manager().call();
+		const players = await lottery.methods.getPlayers().call();
+		const balance = await web3.eth.getBalance(lottery.options.address);
 
-    this.setState({ manager, players, balance });
-    this.checkManager();
-  }
+		this.setState({ manager, players, balance: web3.utils.fromWei(balance, 'ether') });
+		this.checkManager();
+	}
 
-  checkManager = async() => {
-    const accounts = await web3.eth.getAccounts();
-    const manager = await lottery.methods.manager().call();
-    
-    this.setState({isManager: manager === accounts[0]});
-  }
+	setMessage = (message, color, duration) => {
+		this.setState({message, color});
 
-  onSubmit = async (event) => {
-    event.preventDefault();
+		if (duration)
+			setTimeout(() => { this.setState({message: ''}) }, duration * 1000);
+	}
 
-    const accounts = await web3.eth.getAccounts();
+	checkManager = async () => {
+		const accounts = await web3.eth.getAccounts();
+		const manager = await lottery.methods.manager().call();
+		
+		this.setState({isManager: manager === accounts[0]});
+	}
 
-    this.setState({ message: 'Esperando que la transacción se complete ...', color: 'primary' });
+	enterLottery = async (event) => {
+		event.preventDefault();
 
-    await lottery.methods.enter().send({
-      from: accounts[0],
-      value: web3.utils.toWei(this.state.value, 'ether')
-    });
+		const accounts = await web3.eth.getAccounts();
 
-    this.setState({ 
-      message: 'La transacción se completó correctamente. Estás participando de la lotería',
-      color: 'success' 
-    });
+		this.setMessage('Esperando que la transacción se complete ...', 'primary');
 
-    this.updateStats();
-  };
+		await lottery.methods.enter().send({
+			from: accounts[0],
+			value: web3.utils.toWei(this.state.value, 'ether')
+		});
 
-  onClick = async (event) => {
-    const accounts = await web3.eth.getAccounts();
+		this.setMessage('La transacción se completó correctamente. Estás participando de la lotería', 'success', 5);
 
-    this.setState({ message: 'Esperando que la transacción se complete ...', color: 'primary' });
+		this.updateStats();
+	};
 
-    await lottery.methods.selectWinner().send({
-      from: accounts[0]
-    });
+	closeLottery = async (event) => {
+		const accounts = await web3.eth.getAccounts();
 
-    const winner = await lottery.methods.lastWinner().call();
+		this.setMessage('Esperando que la transacción se complete ...', 'primary');
 
-    this.setState({ message: 'Se ha elegido un ganador! Felicitaciones a ' + winner, color: 'success'});
-    this.updateStats();
-  };
+		await lottery.methods.selectWinner().send({
+			from: accounts[0]
+		});
 
-  render() {
-    return (
-      <div>
-        <Container className="my-5">
-          <Row>
-            <Col className="text-center">
-              <h1>Lottery Smart Contract - Neoris Innovation Lab</h1>
-              <p className="lead">
-                El administrador de este contrato es <strong>{ this.state.manager }</strong>.<br/>
-                En este momento hay <strong>{ this.state.players.length }</strong> persona/s compitiendo por 
-                un total de <strong>{ web3.utils.fromWei(this.state.balance, 'ether') }</strong> ether.
-              </p>
-              <small>
-                <a target="_blank" rel="noopener noreferrer" href="https://github.com/br1code">br1code - Github</a>
-              </small>
-            </Col>
-          </Row>
+		const winner = await lottery.methods.lastWinner().call();
 
-          <hr/>
+		this.setMessage('Se ha elegido un ganador! Felicitaciones a ' + winner, 'success', 10);
+		this.updateStats();
+	};
 
-          <Row>
-            <Col className="text-center">
-              <Form onSubmit={ this.onSubmit }>
-                <FormGroup className="mx-3">
-                  <Label className="mr-3 my-2 lead">Cantidad de Ether que quieres apostar</Label>
-                  <Input 
-                    value = { this.state.value }
-                    onChange={ event => this.setState({ value: event.target.value }) }
-                    type="text" placeholder="Ether"
-                    className="w-25 mx-auto my-2"
-                    required/>
-                </FormGroup>
-                <Button className="my-2" color="primary">Participar</Button>
-              </Form>
-            </Col>
-          </Row>
+	render() {
+    	return (
+      		<div>
+        		<Container className="my-5">
+          			<Row>
+            			<Col className="text-center">
+							<h1>Lottery Smart Contract - Neoris Innovation Lab</h1>
+							<p className="lead">
+								El administrador de este contrato es <strong>{ this.state.manager }</strong>.<br/>
+								En este momento hay <strong>{ this.state.players.length }</strong> persona/s compitiendo por 
+								un total de <strong>{ this.state.balance }</strong> ether.
+							</p>
+							<small>
+								<a starget="_blank" rel="noopener noreferrer" href="https://github.com/br1code">br1code - Github</a>
+							</small>
+            			</Col>
+          			</Row>
 
-          <hr/>
+          			<hr/>
 
-          <Row>
-            <Col className="text-center">
-              <h4 className="my-2">Listo para elegir un ganador?</h4>
-              <p className="lead">Solo el administrador puede iniciar la lotería</p>
-              <Button color="danger" onClick={ this.onClick } disabled={!this.state.isManager}>Iniciar la lotería</Button>
-            </Col>
-          </Row>
+					<Row>
+						<Col className="text-center">
+							<Form onSubmit={ this.enterLottery }>
+								<FormGroup className="mx-3">
+									<Label className="mr-3 my-2 lead">Cantidad de Ether que quieres apostar</Label>
+									<Input 
+										value = { this.state.value }
+										onChange={ event => this.setState({ value: event.target.value }) }
+										type="text" placeholder="Ether"
+										className="w-25 mx-auto my-2"
+									required/>
+								</FormGroup>
+								<Button className="my-2" color="primary">Participar</Button>
+							</Form>
+						</Col>
+					</Row>
 
-          <hr/>
+          			<hr/>
 
-          <Alert color={this.state.color} style={{display: this.state.message ? 'block' : 'none' }}>{this.state.message}</Alert>
-        </Container>
-      </div>
-    );
-  }
+					<Row style={{display: this.state.isManager ? 'block' : 'none'}} className="my-3">
+						<Col className="text-center">
+							<h4 className="my-2">Listo para elegir un ganador?</h4>
+							<p className="lead">Solo el administrador puede iniciar la lotería</p>
+							<Button color="danger" onClick={ this.closeLottery }>Iniciar la lotería</Button>
+						</Col>
+					</Row>
+
+          			<Alert color={this.state.color} style={{display: this.state.message ? 'block' : 'none' }}>{this.state.message}</Alert>
+        		</Container>
+      		</div>
+    	);
+  	}
 }
 
 export default App;
--- src/web3.js
+++ src/web3.js
@@ -1,6 +1,6 @@
 import Web3 from 'web3';
 
-// use the Metamask provider to create a new instance of web3
+// create a new instance of web3 using the Metamask provider
 const web3 = new Web3(window.web3.currentProvider);
 
 export default web3;
